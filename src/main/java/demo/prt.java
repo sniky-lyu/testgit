2c1700841960ce5523d23836a7d1163a4b0a54b0
sniky-lyu <junkklv@outlook.com>
2020-01-10 22:43:19 +0800

    Initial commit

---
+++ src/main/java/demo/prt.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * version-1
+ */
+public class prt {
+    public static void main(String[] args) {
+        System.out.println("this is a test for git integration.");
+    }
+}
