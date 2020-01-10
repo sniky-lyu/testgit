9418e01dc9cf46743e4ee9d342786e032a4f8a87
sniky-lyu <junkklv@outlook.com>
2020-01-10 23:25:58 +0800

    add .csv in ignore and add sout for version-2

--- src/main/java/demo/prt.java
+++ src/main/java/demo/prt.java
@@ -3,10 +3,12 @@ package demo;
 /**
  * version-1
  * --> version-2
+ * --> version-3
  */
 public class prt {
     public static void main(String[] args) {
         System.out.println("this is a test for git integration.");
         System.out.println("test for git integration: version-2");
+        System.out.println("test for git integration: version-3");
     }
 }
